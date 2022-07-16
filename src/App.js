8083a02e1dcfc3786a1f4fb2ea495997537ebeeb
Emanuele Simula <simulaemanuele@gmail.com>
2022-07-16 22:29:15 +0200

    exercise: basic-concepts/components-03 done

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { HelloWorld } from "./HelloWorld";
+
+export class App extends React.Component{
+    render(){
+        <HelloWorld />
+    }
+}
--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export class HelloWorld extends React.Components{
+export class HelloWorld extends React.Component{
     render(){
         return <h1>Hello, World!!</h1>
     }
