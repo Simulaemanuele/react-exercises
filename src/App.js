53135829cc809fac0d16511cda12e7eaf9043e45
Emanuele Simula <simulaemanuele@gmail.com>
2022-07-17 01:29:37 +0200

    update branch

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { HelloWorld } from "./HelloWorld";
+
+export class App extends React.Component{
+    render(){
+        return(
+        <div>
+            <HelloWorld />
+            </div>
+        )
+    }
+}
