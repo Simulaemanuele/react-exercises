71affb262e3e15b0f7440b95acd9811d72fe6302
Emanuele Simula <simulaemanuele@gmail.com>
2022-07-17 01:37:00 +0200

    exercise: basic-concepts/props-01 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { HelloWorld } from "./HelloWorld";
+import { Welcome } from "./Welcome";
 
 export class App extends React.Component{
     render(){
         return(
         <div>
             <HelloWorld />
+            <Welcome name='Emanuele' />
             </div>
         )
     }
