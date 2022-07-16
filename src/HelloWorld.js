5699cc122d234fc29efe2d7455836de99cbea216
Emanuele Simula <simulaemanuele@gmail.com>
2022-07-16 22:57:37 +0200

    exercise: basic-concepts/components-06 done

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -4,6 +4,8 @@ export class HelloWorld extends React.Component {
     render() {
         return (<div>
             <h1>Hello, World!!</h1>
+
+            <p>What a beautiful day!!</p>
         </div>)
     }
 }
