884e6f652f23d6f455433684168758390b1dc2a5
Emanuele Simula <simulaemanuele@gmail.com>
2022-07-17 22:25:05 +0200

    exercise: basic-concepts/conditional-rendering-02 done

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Age } from "./Age";
 
 export class Welcome extends React.Component {
-    render(){
+    render() {
         return (
             <div>
                 <p>Hello, {this.props.name ?? 'Ema'}!</p>
-                <Age age= {this.props.age > 18 && this.props.age} />
+
+                {this.props.age && <Age age={this.props.age > 18 && this.props.age} />}
             </div>
         )
     }
