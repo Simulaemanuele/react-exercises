6872ebcc65d9d50194bfdfa35fbe7653f428de48
Emanuele Simula <simulaemanuele@gmail.com>
2022-07-17 02:09:09 +0200

    exercise: basic-concepts/props-05 done

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Age } from "./Age";
 
 export class Welcome extends React.Component {
     render(){
         return (
             <div>
                 <p>Hello, {this.props.name ?? 'Ema'}!</p>
-                <p>Your age is {this.props.age}</p>
+                <Age age= {this.props.age} />
             </div>
         )
     }
