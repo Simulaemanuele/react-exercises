4766251cadc3037ca600071e42412f86d1de3081
Emanuele Simula <simulaemanuele@gmail.com>
2022-08-01 10:41:08 +0200

    mod: modified in Sum.js <p> tag into <h1> tag

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -4,7 +4,7 @@ export function Sum({numbers}){
     return(
         <div>
             <p>The numbers are {numbers}</p>
-            <p>The sum of all numbers is: {numbers.reduce((number, sum) => sum + number,0)}</p>
+            <h1>The sum of all numbers is: {numbers.reduce((number, sum) => sum + number,0)}</h1>
         </div>
     )
 }
