9fa85252ac1ef5a6c08f09797191de6d5be9765b
Emanuele Simula <simulaemanuele@gmail.com>
2022-08-01 18:33:19 +0200

    exercise: Function-component-and-hooks/functionComponents-03 done

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component{
     render(){
         return(
             <div className="mainContainer">
-                <Sum numbers={[1,2,3,4,5,6,7,8,9]}/>
+                <Sum />
             </div>
         )
     }
--- src/components/Sum.js
+++ src/components/Sum.js
@@ -1,4 +1,4 @@
-export function Sum({numbers}){
+export function Sum({numbers = [1,2,3,4,5,6,7,8,9]}){
 
     
     return(
