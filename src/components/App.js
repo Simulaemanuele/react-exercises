6002a86d612246afbd04bfe97f1aedc3ce8359d7
Emanuele Simula <simulaemanuele@gmail.com>
2022-08-01 09:45:56 +0200

    exercise: Function-components-and-Hooks/functionComponents-01 done

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,13 @@
 import React from "react";
-import { LanguageContext } from "./LanguageContext";
-import { DisplayLanguage } from "./DisplayLanguage";
+import { Welcome } from "./Welcome";
 
 export class App extends React.Component{
-    state = {
-        language: 'en',
-    }
-
-    handleLanguageChange = (event) => {
-        this.setState({
-            language: event.target.value,
-        })
-    }
+    
     render(){
         return(
-            
-        <LanguageContext.Provider value={this.state.language}>
-            <select value={this.state.language} onChange={this.handleLanguageChange}>
-                <option value='en'>ENGLISH</option>
-                <option value='it'>ITALIANO</option>
-            </select>
-            <DisplayLanguage />
-            
-        </LanguageContext.Provider>
+            <div className="mainContainer">
+                <Welcome name="Emanuele"/>
+            </div>
         )
     }
 }
