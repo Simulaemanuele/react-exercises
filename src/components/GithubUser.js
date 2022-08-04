05ccc4be4c8b71b7fac662123e64612f1a285d95
Emanuele Simula <simulaemanuele@gmail.com>
2022-08-05 00:17:54 +0200

    fixed useEffect-03, maybe?

--- src/components/GithubUser.js
+++ src/components/GithubUser.js
@@ -31,7 +31,7 @@ export function GithubUser({ username }) {
         setLoading(true)
 
         try{
-            const response = await fetch('https://api.github.com/users/{username}')
+            const response = await fetch(`https://api.github.com/users/${username}`)
             const json = await response.json()
 
             setData(json)
