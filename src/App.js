78431bc7a9cda4e5b1791ad8d29f26ca556da6ee
Emanuele Simula <simulaemanuele@gmail.com>
2022-08-06 13:38:11 +0200

    exercise: Function-components-and-Hooks/useMemo done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
-import { ClickCounter } from "./components/ClickCounter";
+import { FilteredList } from "./components/FilteredList";
 
 
+const nameList = [
+    { name: 'Gianni', id: 1, age: 22 },
+    { name: 'Paolo', id: 2, age: 28 },
+    { name: 'Carlo', id: 3, age: 18 },
+    { name: 'Maria', id: 4, age: 15 },
+    { name: 'Gianandrea', id: 5, age: 32 },
+    { name: 'Marco', id: 6, age: 27 },
+    { name: 'Giorgio', id: 7, age: 17 },
+  ];
 
 
 
 
 export function App() {
     
-
-
-
-
     return (
         <div className="mainContainer">
-            <ClickCounter />
+            <FilteredList list={nameList}/>
         </div>
     )
 
