9b8b34bdab8df7efbc2770de42ad4396481f93a9
Emanuele Simula <simulaemanuele@gmail.com>
2022-08-08 13:33:08 +0200

    exercise: External-Libreries/react-router-03

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom"
 import { Counter } from "./components/Counter";
+import { ShowGithubUser } from "./components/ShowGithubUser";
 import { Welcome } from "./components/Welcome";
 
 
@@ -16,6 +17,7 @@ export function App() {
         <Routes>
             <Route path="/" element={<Welcome />} />
             <Route path="/counter" element={<Counter/>}/>
+            <Route path="/user/:username" element={<ShowGithubUser />}/>
         </Routes>
     )
 
---
+++ src/components/ShowGithubUser.js
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom"
+import { GithubUser } from "./GithubUser"
+
+export function ShowGithubUser(){
+    const {username} = useParams()
+    return(
+        <div>
+            <GithubUser username={username} />
+        </div>
+    )
+}
