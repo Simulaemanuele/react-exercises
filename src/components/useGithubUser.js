f0850370305538f380aabcdb171c695da13f7c5d
Emanuele Simula <simulaemanuele@gmail.com>
2022-08-10 00:55:58 +0200

    exercise: External-Libraries/swr-02 done

--- src/components/useGithubUser.js
+++ src/components/useGithubUser.js
@@ -3,7 +3,7 @@ import useSWR from 'swr';
 const fetcher = url => fetch(url).then(response => response.json());
 
 export function useGithubUser(username){
-    const {data, error} = useSWR(`https://api.github.com/users/${username}`, fetcher);
+    const {data, error} = useSWR(username ? `https://api.github.com/users/${username}` : null, fetcher);
 
     
     return{ users: data, error, isloading: !data && !error, }
