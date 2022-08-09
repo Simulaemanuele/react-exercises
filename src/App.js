63414783725a168a48226124d013c663c121ba20
Emanuele Simula <simulaemanuele@gmail.com>
2022-08-09 23:28:17 +0200

    exercise: External-Libraries/react-router-07 done

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ export function App() {
                 <Route path="/" element={<Welcome />} />
                 <Route path="/counter" element={<Counter />} />
                 <Route path="/user" element={<GithubUserList />} >
+                    <Route index element={<h3>Add a user and select it</h3>}/>
                     <Route path=":username" element={<ShowGithubUser />} />
                 </Route>
             </Routes>
